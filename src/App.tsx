9886a6d22b4944fca23bc48a08ebc0feb08c9171
kayheypark <kayheypark@gmail.com>
2022-02-15 22:12:46 +0900

    코드정리

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,22 @@ import About from './pages/About';
 function App() {
   return (
     <>
-      
       <header>
-
         <nav>
           <ul>
             <li>
-              <Link to='/'>홈</Link>
+              <Link to="/">홈</Link>
             </li>
             <li>
-              <Link to='/about'>어바웃</Link>
+              <Link to="/about">어바웃</Link>
             </li>
           </ul>
         </nav>
-
       </header>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
       <footer>푸터</footer>
