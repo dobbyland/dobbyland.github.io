07a113647f242ab3b4bcf8acee8c3fa0dd1715f6
kayheypark <kayheypark@gmail.com>
2022-02-28 23:32:20 +0900

    우리는요 코드정리

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,11 @@
 const About = () => {
-    return (
-        <>
-            <p>
-              안녕하세요 <strong>도비랜드</strong>에 오신 것을 환영합니다!
-            </p>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <p>안녕하세요 <strong>도비랜드</strong>에 오신 것을 환영합니다!</p>
+      </div>
+    </>
+  )
 };
 
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import Button from 'components/atoms/Button';
-
 const Home = () => {
     return (
         <>
             <p>홈입니다.</p>
-            <Button color={`blue`} onClick={()=>alert(1)}>버튼</Button>
         </>
     )
 };
